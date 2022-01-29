9f711fca2930a7ccbf04985672d1e15c70e5e836
Robert Burke <rwburke@me.com>
2022-01-29 03:13:18 -0500

    yes

--- spotifyIntegration/main.py
+++ spotifyIntegration/main.py
@@ -1,79 +1,110 @@
 import requests
 import time
-
+from threading import Thread
 from pprint import pprint
 
 
 SPOTIFY_GET_CURRENT_TRACK_URL = 'https://api.spotify.com/v1/me/player/currently-playing'
 SPOTIFY_GET_TRACK_INFO_URL = 'https://api.spotify.com/v1/audio-analysis/'
-ACCESS_TOKEN = 'BQCav-MNCUqWSWTGK_JsnvsF7fTZHHVH9LDnhzVbJtTJ-HpgumZ21-yOQA3LDYszJpkHKPQF_pOYF0PBYOxLY3pMYUVY1cDdhlGrRDj0ZuUzgJdEY7S92fYwNVPpa6sGgQg2I8_9kEycUfOQ-CRVLNoKDODof-_ZzqJUec7VBUFVRwiA2w'
+ACCESS_TOKEN = 'BQCHPoeTyjz31W_NtkY-DKEKFT5vyC9pd5Wm2T5oMjEC5juR70hi5wm4Ncct8pgMhiQAQbkPjILHiqW484DWzdNrd8Mde6kPCfyUkt9al-h5iPGi9N1I_CV7dr3OKeoMiTCxhuuL4N6Jh1YWClkoTQJnF4rbng5rdcVcKuDkiBCtq0nd1A'
 
 previous_track_id = ''
-track_json = {}
-track_prev_loc = 0
+track_json_segments = []
 
-def get_current_track(access_token):
-    global previous_track_id
-    global track_json 
-    global track_prev_loc
+# in ms
+current_track_time = 0
+current_local_time = 0
+current_track_segment = 0
 
-    response = requests.get(
-        SPOTIFY_GET_CURRENT_TRACK_URL,
-        headers={
-            "Authorization": f"Bearer {access_token}"
-        }
-    )
-    current_track_info = {}
-    pprint(response.status_code)
-    if response.status_code == 200:
-        json_resp = response.json()
+def spike(x, n):
+    return 1 - abs((x - n*8.67)/4.941)
 
-        track_id = json_resp['item']['id']
+def get_color(j, loudness, pitches):
+    pitches.append(pitches[0])
+    return loudness* (pitches[int(j/8.67)] * spike(j, int(j/8.67)) + pitches[int(j/8.67)] * spike(j, int(j/8.67) + 1))
 
-        link = json_resp['item']['external_urls']['spotify']
-        position = json_resp['progress_ms']
-
-        current_track_info = {
-            "link": link,
-            "position": position
-        }
-
-        if previous_track_id != track_id:
-            trackinfo = requests.get(
-                SPOTIFY_GET_TRACK_INFO_URL + track_id, 
-                headers={
-                    "Authorization": f"Bearer {access_token}"
-                }
-            ) 
-            
-            pprint(trackinfo.status_code)
-            if trackinfo.status_code == 200:
-                track_json = trackinfo.json() 
-
-        i = track_prev_loc
-        while i < len(track_json['segments']) and position/1000 > track_json['segments'][i]['start'] + track_json['segments'][i]['duration']:
-            i += 1 
-                
-        if i == len(track_json['segments']):
-            i = 0 
-            while i < len(track_json['segments']) and position/1000 > track_json['segments'][i]['start'] + track_json['segments'][i]['duration']:
-                i += 1 
-
-        track_prev_loc = i
-
-        pprint(track_json['segments'][i])
-
-    return current_track_info
-
-
-def main():
+def get_current_segment():
+    global current_track_segment
+    temptime = current_track_time + current_local_time - time.time()
+    left = 0
+    right = len(track_json_segments) - 1
+    if track_json_segments[current_track_segment]['start'] + track_json_segments[current_track_segment]['duration'] > temptime and track_json_segments[current_track_segment]['start'] < temptime:
+        return
+    if left == right:
+        current_track_segment = left
+        return 
     while True:
-        current_track_info = get_current_track(ACCESS_TOKEN)
+        mid = int((left + right)/2)
+        if track_json_segments[mid]['start'] <= temptime:
+            left = mid
+            if track_json_segments[mid]['start'] + track_json_segments[mid]['duration'] > temptime:
+                current_track_segment = mid
+                break
+        if track_json_segments[mid]['start'] > temptime:
+            right = mid
 
-        pprint(
-            current_track_info,
-            indent=4,
+def get_loudness():
+    segment = track_json_segments[current_track_segment]
+    if current_track_segment < len(track_json_segments) - 1:
+        temptime = current_track_time + current_local_time - time.time()
+        temp = 0
+        if temptime < segment['loudness_max'] + segment['loudness_max_time']:
+            temp = (temptime - segment['start']) * (segment['loudness_max'] - segment['loudness_start'])/segment['loudness_max_time'] + segment['loudness']
+        else:
+            segment2 = track_json_segments[current_track_segment + 1]
+            temp = (temptime - segment['start'] - segment['loudness_max_time']) * (segment2['loudness_start'] - segment['loudness_max'])/(segment['duration'] - segment['loudness_max_time']) + segment['loudness_max']
+
+        return (temp+60)/60
+    else:
+        return 0
+
+def track_updater(access_token):
+    global previous_track_id
+    global track_json_segments
+    global current_track_time
+    global current_local_time
+
+    while True:
+        response = requests.get(
+            SPOTIFY_GET_CURRENT_TRACK_URL,
+            headers={
+                "Authorization": f"Bearer {access_token}"
+            }
         )
+        if response.status_code != 200:
+            print("bad user read ", response.status_code)
+        if response.status_code == 200:
+            json_resp = response.json()
+            if json_resp == None:
+                continue
+            track_id = json_resp['item']['id']
+            current_track_time = json_resp['progress_ms']/1000
+            current_local_time = time.time()
+
+            if previous_track_id != track_id:
+                trackinfo = requests.get(
+                    SPOTIFY_GET_TRACK_INFO_URL + track_id, 
+                    headers={
+                        "Authorization": f"Bearer {access_token}"
+                    }
+                ) 
+
+                if trackinfo.status_code == 200:
+                    track_json_segments = trackinfo.json()["segments"]
+                    previous_track_id = track_id
+                else: 
+                    print("bad analysis read ", trackinfo.status_code)
+        time.sleep(1)
+
+def main(): 
+    t = Thread(target=track_updater, args=[ACCESS_TOKEN])
+    t.daemon = False
+    t.start()
+    while True: 
+        if len(track_json_segments) > 0:
+            get_current_segment()
+            loud = get_loudness()
+            get_color(0, loud, track_json_segments[current_track_segment]['pitches'])
 
 
 if __name__ == '__main__':
